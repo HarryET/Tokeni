48c4cd4679ea11d8e1806515ea6b5fec31ae0e1b
HarryET <hazzeryog@gmail.com>
2020-09-08 15:12:53 +0100

    Updates to package

--- dist/index.js
+++ dist/index.js
@@ -1,44 +1,21 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var crypto = __importStar(require("crypto"));
 var Tokeni = /** @class */ (function () {
     function Tokeni() {
     }
-    Tokeni.encrypt = function (identifier, data, key) {
+    Tokeni.encrypt = function (identifier, validator, key) {
         var p = [];
         p.push(Buffer.from(identifier).toString('base64'));
         p.push(Buffer.from((~~(Date.now() / 1000) - 1155945600).toString()).toString('base64'));
-        p.push(crypto.createHmac("sha1", key).update(JSON.stringify(data)).digest("base64"));
+        p.push(Buffer.from(validator).toString('base64'));
         return p.join(".");
     };
-    Tokeni.validate = function (token, data, key) {
+    Tokeni.decrypt = function (token, key) {
         var ts = token.split(".");
-        var p = [];
-        p.push(ts[0]);
-        p.push(ts[1]);
-        p.push(crypto.createHmac("sha1", key).update(JSON.stringify(data)).digest("base64"));
         return {
-            valid: p.join(".") == token,
-            identifier: Buffer.from(ts[0], 'base64').toString()
+            identifier: Buffer.from(ts[0], 'base64').toString('utf-8'),
+            date: new Date(Buffer.from(ts[1], 'base64').readInt32BE(0)),
+            validator: ts[2]
         };
     };
     return Tokeni;
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,31 @@
-import * as crypto from "crypto";
-
 type validateRes = {
-    valid: boolean;
     identifier: string;
+    date: Date;
+    validator: any;
 }
+
 
 export default class Tokeni {
 
-    static encrypt(identifier: string, data: object, key: string): string {
+    static encrypt(identifier: string, validator: any, key: string): string {
         let p = [];
 
         p.push( Buffer.from(identifier).toString('base64') );
         p.push( Buffer.from((~~(Date.now() / 1000) - 1155945600).toString()).toString('base64') );
-        p.push( crypto.createHmac("sha1", key).update(JSON.stringify(data)).digest("base64") );
+        p.push( Buffer.from( validator ).toString('base64') );
 
         return p.join(".");
     }
 
-    static validate(token: string, data: object, key: string): validateRes {
+    static decrypt(token: string, key: string): validateRes {
         let ts = token.split(".")
-        let p = [];
-
-        p.push( ts[0] );
-        p.push( ts[1] );
-        p.push( crypto.createHmac("sha1", key).update(JSON.stringify(data)).digest("base64") );
 
         return {
-            valid: p.join(".") == token,
-            identifier: Buffer.from(ts[0], 'base64').toString()
+            identifier: Buffer.from(ts[0], 'base64').toString('utf-8'),
+            date: new Date(Buffer.from(ts[1], 'base64').readInt32BE(0)),
+            validator: ts[2]
         };
 
     }
 
 }
-
